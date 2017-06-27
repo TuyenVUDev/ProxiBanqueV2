eb1cbc6c990d9f462a39485e187f7e8c2b5cc219
TuyenVUDev <tuyentown@gmail.com>
2017-06-27 10:06:57 +0200

    ajout de fonctionnalites listes de comptes par conseiller

--- src/main/java/fr/gtm/proxibanque/service/ConseillerCompteCRUDService.java
+++ src/main/java/fr/gtm/proxibanque/service/ConseillerCompteCRUDService.java
@@ -7,35 +7,53 @@ import fr.gtm.proxibanque.domain.Conseiller;
 import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 import fr.gtm.proxibanque.dao.ConseillerClientCRUDDao;
 import fr.gtm.proxibanque.dao.ConseillerCompteCRUDDao;
 
 public class ConseillerCompteCRUDService implements CRUDInterface {
 
+	ConseillerCompteCRUDDao conseillerCompteCRUDDao = new ConseillerCompteCRUDDao();
+	ConseillerClientCRUDService conseillerClientCRUDService = new ConseillerClientCRUDService();
 
-ConseillerCompteCRUDDao conseillerCompteCRUDDao =new ConseillerCompteCRUDDao();	
-	
 	public boolean ajout(Compte compte) {
 		conseillerCompteCRUDDao.ajout(compte);
 		return true;
 	}
 
-	public ArrayList<Compte> lire(){
+	public ArrayList<Compte> lire() {
 		return conseillerCompteCRUDDao.lireListe();
-		
+
 	};
+
 	public Compte lireById(int id) {
 		return conseillerCompteCRUDDao.lireById(id);
 	}
-	
-	
+
 	public boolean purge() {
 		return conseillerCompteCRUDDao.purgeTable();
 	}
 
 	public boolean suppression(int id) {
 		return conseillerCompteCRUDDao.suppression(id);
+	}
+
+	public ArrayList<Integer> getIdCompteCourantAvecIdConseiller(int idConseiller) {
+		return (ArrayList<Integer>) conseillerClientCRUDService.lire(idConseiller).stream().map(c -> c.getIdCompteCourant())
+				.filter(c -> c != 0).collect(Collectors.toList());
+	}
+
+	public ArrayList<Integer> getIdCompteEpargneAvecIdConseiller(int idConseiller) {
+		return (ArrayList<Integer>) conseillerClientCRUDService.lire(idConseiller).stream().map(c -> c.getIdCompteEpargne())
+				.filter(c -> c != 0).collect(Collectors.toList());
+	}
+
+	public ArrayList<Integer> getIdToutCompteAvecIdConseiller(int idConseiller) {
+		ArrayList compteParConseiller = getIdCompteCourantAvecIdConseiller(idConseiller);
+		compteParConseiller.addAll(getIdCompteEpargneAvecIdConseiller(idConseiller));
+		return compteParConseiller;
 	}
 
 }
